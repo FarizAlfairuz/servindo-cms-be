d08294de5d10824d0c00ae562e2698484d257047
Fariz <farizalfairuz27@gmail.com>
2022-06-12 11:43:01 +0700

    add: ssl certificate

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const express = require('express')
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const path = require('path')
+
 const passport = require('./utils/passport')
 
 const sessionConfig = require('./config/sessionConfig')
@@ -12,14 +17,24 @@ const { connectDB, sequelize } = require('./utils/database')
 const routes = require('./routes')
 
 const app = express()
-const port = process.env.PORT || 8000
+const httpPort = process.env.HTTP_PORT || 8080
+const httpsPort = process.env.HTTPS_PORT || 8443
+const privateKey = fs.readFileSync(path.join(__dirname, '../cert/private.key'))
+const certificate = fs.readFileSync(
+  path.join(__dirname, '../cert/certificate.crt')
+)
+
 const sessionStore = new SequelizeStore({
   db: sequelize,
 })
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
   credentials: true,
-  optionSuccessStatus: 200
+  optionSuccessStatus: 200,
+}
+const sslOptions = {
+  key: privateKey,
+  cert: certificate,
 }
 sessionStore.sync()
 
@@ -36,8 +51,11 @@ Object.keys(routes).forEach((route) => {
   app.use('/api', routes[route])
 })
 
-connectDB().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-  })
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(sslOptions, app)
+
+connectDB().then(async () => {
+  await httpServer.listen(httpPort)
+  await httpsServer.listen(httpsPort)
+  console.log(`Server is running on port ${httpPort} & ${httpsPort}`)
 })
